e89b16a738ddf3a68c02c0523a43fa6ef31d73cc
Matej Franjić <mfranjic21@foi.hr>
2023-11-08 15:03:19 +0100

    korisnicka_podrska view kreirani grubo postavljeni gumbovi/tekstovi, KorisnickaPodrska klasa kreirane

---
+++ Software/app/src/main/java/com/example/iznajmljivanjevozila/KorisnickaPodrska.kt
@@ -0,0 +1,4 @@
+package com.example.iznajmljivanjevozila
+
+class KorisnickaPodrska {
+}
--- Software/app/src/main/java/com/example/iznajmljivanjevozila/MainActivity.kt
+++ Software/app/src/main/java/com/example/iznajmljivanjevozila/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.iznajmljivanjevozila
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.iznajmljivanjeauta.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
