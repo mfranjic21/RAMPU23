1b24b693dfb6d5115231ac81f17f61f94e67c158
Matej Franjić <mfranjic21@foi.hr>
2023-11-08 15:50:47 +0100

    Povezivanje korisnicka_podrska.xml s activity_main.xml

--- Software/app/src/main/java/com/example/iznajmljivanjevozila/KorisnickaPodrska.kt
+++ Software/app/src/main/java/com/example/iznajmljivanjevozila/KorisnickaPodrska.kt
@@ -1,4 +1,18 @@
 package com.example.iznajmljivanjevozila
 
-class KorisnickaPodrska {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.iznajmljivanjevozila.R
+
+class KorisnickaPodrska : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.korisnicka_podrska)
+
+        findViewById<Button>(R.id.natrag).setOnClickListener {
+            startActivity(Intent (this, MainActivity::class.java))
+        }
+    }
 }
--- Software/app/src/main/java/com/example/iznajmljivanjevozila/MainActivity.kt
+++ Software/app/src/main/java/com/example/iznajmljivanjevozila/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.iznajmljivanjevozila
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.iznajmljivanjeauta.R
+import android.widget.Button
+import com.example.iznajmljivanjevozila.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val korisnickaPodrska = findViewById<Button>(R.id.korisnickaPodrska)
+
+        korisnickaPodrska.setOnClickListener {
+            val intent1 = Intent (this, KorisnickaPodrska::class.java)
+            startActivity(intent1)
+        }
     }
 }
