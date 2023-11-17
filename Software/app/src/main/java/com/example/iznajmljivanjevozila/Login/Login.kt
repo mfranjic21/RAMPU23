46a9bb57dd78bf486f88de383015169d29cd510e
msasko1 <msasko1@foi.hr>
2023-11-17 22:04:33 +0100

    Kreiran objekt za upravljanje sa sesijom, dodana data klasa User i implementirano prebacivanje s pocetnog zaslona na zaslona za prijavu ako korisnik nije prijavljen #3

---
+++ Software/app/src/main/java/com/example/iznajmljivanjevozila/Login/Login.kt
@@ -0,0 +1,12 @@
+package com.example.iznajmljivanjevozila.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.iznajmljivanjevozila.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
