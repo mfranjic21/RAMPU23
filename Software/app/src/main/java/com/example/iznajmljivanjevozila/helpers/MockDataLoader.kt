97b70b83efff101a1d52eff053ace746b222e709
msasko1 <msasko1@foi.hr>
2023-11-18 01:44:33 +0100

    Nadograden MockDataLoader da ima funkcije koje rade s singleton objektom UsersList, implementirana registracija s jednostavnom provjerom unosa.  #3

--- Software/app/src/main/java/com/example/iznajmljivanjevozila/helpers/MockDataLoader.kt
+++ Software/app/src/main/java/com/example/iznajmljivanjevozila/helpers/MockDataLoader.kt
@@ -4,10 +4,25 @@ import com.example.iznajmljivanjevozila.data.User
 
 object MockDataLoader {
     fun getUsers(): List<User> =  UsersList.users.toList()
+
+    fun existsEmail(email:String): Boolean{
+        val users = UsersList.users
+        return users.any{user -> user.email == email}
+    }
+
+    fun existsUsername(username:String): Boolean{
+        val users = UsersList.users
+        return users.any{user -> user.username == username}
+    }
+
+    fun addUser(user : User){
+        val users = UsersList.users
+        users.add(user)
+    }
 }
 
 object UsersList{
-    var users: List<User> = mutableListOf(
+    var users: MutableList<User> = mutableListOf(
         User("test_ime","test_prezime", "test@example.com", "test" , "test")
     )
 }
