4f9a6b6b19c402486bdff15a57f6d71ff220393f
Marko Saško <msasko1@foi.hr>
2024-01-22 23:29:36 +0100

    Implementirano pretraživanje. Na događaj onQueryTextSubmit searchViewa poziva se metoda za filtriranje koja prima tip filtera i samu vrijednost filtera. U listi vozila ostaju ona koja zadovoljavaju filteri obaviještava se adapter prikaza vozila da se osviježio set podataka #4

--- Software/app/src/main/java/com/example/iznajmljivanjevozila/MainActivity.kt
+++ Software/app/src/main/java/com/example/iznajmljivanjevozila/MainActivity.kt
@@ -9,6 +9,7 @@ import android.widget.ArrayAdapter
 import com.example.iznajmljivanjevozila.Login.Login
 import com.example.iznajmljivanjevozila.Login.SessionManager
 import android.widget.ImageButton
+import android.widget.SearchView
 import android.widget.Spinner
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -17,13 +18,15 @@ import com.example.iznajmljivanjevozila.carsDB.Cars
 import com.example.iznajmljivanjevozila.carsDB.carsList
 
 class MainActivity : AppCompatActivity() {
-    var currentFilter: String = "Marka"
+    private lateinit var carList : RecyclerView
+    private var currentFilter: String = "Marka"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fillCarView()
-        fillFilterOptions()
+        if(carsList.size == 0) {
+            fillCarView()
+        }
 
         if(!SessionManager.isLoggedIn()){
             val intent = Intent(this, Login::class.java)
@@ -37,21 +40,21 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent (this, Profil::class.java)
             startActivity(intent)
         }
-
-        val carList = findViewById<RecyclerView>(R.id.carReservation)
+        carList = findViewById(R.id.carReservation)
         carList.layoutManager = LinearLayoutManager(this)
         carList.adapter = CarListAdapter(carsList)
+
+        fillFilterOptions()
+        setSearchViewListener()
     }
 
     private fun fillCarView() {
-        if(carsList.size == 0){
-            carsList.add(Cars("Ford", "Focus", "2017", 4.5, "PŽ-1125-GS", "149,550 km", "Dizel, Manualni mjenjač, Broj sjedala: 5", true, "", R.drawable.ford_focus))
-            carsList.add(Cars("Škoda", "Octavia", "2022", 4.9, "OS-5694-DK", "4,000 km", "Dizel, Automatski mjenjač, Broj sjedala:5", true, "", R.drawable.skoda_octavia))
-            carsList.add(Cars("Audi", "A4", "2009", 3.9, "PŽ-452-FG", "140,382 km", "Benzin, Manualni mjenjač, Broj sjedala: 5", true, "", R.drawable.audi_a4))
-            carsList.add(Cars("Volkswagen", "Tiguan", "2019", 4.2, "VŽ-2152-KT", "67,415 km", "Dizel, Automatski mjenjač, Broj sjedala:5", true, "", R.drawable.vw_tiguan))
-            carsList.add(Cars("Mercedes-Benz", "C-class", "2018", 4.3, "VŽ-2152-KT", "90,419 km", "Dizel, Automatski mjenjač, Broj sjedala:4", true, "", R.drawable.mercedesbenz_cclass))
-            carsList.add(Cars("Volvo", "V40", "2016", 3.7, "SB-125-BN", "110,792 km", "Dizel, Manualni mjenjač, Broj sjedala:5", true, "", R.drawable.volvo_v40))
-        }
+        carsList.add(Cars("Ford", "Focus", "2017", 4.5, "PŽ-1125-GS", "149,550 km", "Dizel, Manualni mjenjač, Broj sjedala: 5", true, "", R.drawable.ford_focus))
+        carsList.add(Cars("Škoda", "Octavia", "2022", 4.9, "OS-5694-DK", "4,000 km", "Dizel, Automatski mjenjač, Broj sjedala:5", true, "", R.drawable.skoda_octavia))
+        carsList.add(Cars("Audi", "A4", "2009", 3.9, "PŽ-452-FG", "140,382 km", "Benzin, Manualni mjenjač, Broj sjedala: 5", true, "", R.drawable.audi_a4))
+        carsList.add(Cars("Volkswagen", "Tiguan", "2019", 4.2, "VŽ-2152-KT", "67,415 km", "Dizel, Automatski mjenjač, Broj sjedala:5", true, "", R.drawable.vw_tiguan))
+        carsList.add(Cars("Mercedes-Benz", "C-class", "2018", 4.3, "VŽ-2152-KT", "90,419 km", "Dizel, Automatski mjenjač, Broj sjedala:4", true, "", R.drawable.mercedesbenz_cclass))
+        carsList.add(Cars("Volvo", "V40", "2016", 3.7, "SB-125-BN", "110,792 km", "Dizel, Manualni mjenjač, Broj sjedala:5", true, "", R.drawable.volvo_v40))
     }
 
     private fun fillFilterOptions(){
@@ -70,6 +73,54 @@ class MainActivity : AppCompatActivity() {
             override fun onNothingSelected(parentView: AdapterView<*>?) {
             }
         })
+    }
+
+    private fun setSearchViewListener(){
+        val searchView = findViewById<SearchView>(R.id.main_search_bar)
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if(query != null) {
+                    filterCarView(query, currentFilter)
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if(newText == ""){
+                    carsList.clear()
+                    fillCarView()
+                    carList.adapter?.notifyDataSetChanged()
+                }
+                return true
+            }
+        })
+    }
+
+    private fun filterCarView(filterValueArg: String, filterType: String){
+        val filterValue = filterValueArg.lowercase()
+        val filter: Array<Pair<String, String>> = arrayOf(
+            Pair("Marka", "mark"),
+            Pair("Model", "model"),
+            Pair("Godina", "year"),
+            Pair("Kilometraža do", "currentMileage")
+        )
+
+        val actualFilter = filter.firstOrNull { it.first == filterType }?.second
+
+        carsList.retainAll { car ->
+            when (actualFilter) {
+                "mark" -> car.mark.contains(filterValue, ignoreCase = true)
+                "model" -> car.model.contains(filterValue, ignoreCase = true)
+                "year" -> car.year.contains(filterValue, ignoreCase = true)
+                "currentMileage" -> {
+                    val numericValue = car.currentMileage.replace("[^\\d]".toRegex(), "").toIntOrNull()
+                    numericValue != null && numericValue < filterValue.toInt()
+                }
+                else -> false
+            }
+        }
+        carList.adapter?.notifyDataSetChanged()
     }
 
 }
