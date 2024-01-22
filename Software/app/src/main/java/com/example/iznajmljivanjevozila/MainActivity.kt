02f2354d4b6b2f0283bad6aafee7574d17ddbd8a
Marko Saško <msasko1@foi.hr>
2024-01-22 22:22:25 +0100

    Dodano popunjavanje liste mogućih filtera kao i mijenjanje globalne varijable koja sadrži njegovu vrijednost na događaj onItemSelected filter Spinnera #4

--- Software/app/src/main/java/com/example/iznajmljivanjevozila/MainActivity.kt
+++ Software/app/src/main/java/com/example/iznajmljivanjevozila/MainActivity.kt
@@ -3,9 +3,13 @@ package com.example.iznajmljivanjevozila
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.example.iznajmljivanjevozila.Login.Login
 import com.example.iznajmljivanjevozila.Login.SessionManager
 import android.widget.ImageButton
+import android.widget.Spinner
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.iznajmljivanjevozila.carsDB.CarListAdapter
@@ -13,11 +17,13 @@ import com.example.iznajmljivanjevozila.carsDB.Cars
 import com.example.iznajmljivanjevozila.carsDB.carsList
 
 class MainActivity : AppCompatActivity() {
+    var currentFilter: String = "Marka"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         fillCarView()
+        fillFilterOptions()
 
         if(!SessionManager.isLoggedIn()){
             val intent = Intent(this, Login::class.java)
@@ -46,6 +52,24 @@ class MainActivity : AppCompatActivity() {
             carsList.add(Cars("Mercedes-Benz", "C-class", "2018", 4.3, "VŽ-2152-KT", "90,419 km", "Dizel, Automatski mjenjač, Broj sjedala:4", true, "", R.drawable.mercedesbenz_cclass))
             carsList.add(Cars("Volvo", "V40", "2016", 3.7, "SB-125-BN", "110,792 km", "Dizel, Manualni mjenjač, Broj sjedala:5", true, "", R.drawable.volvo_v40))
         }
+    }
+
+    private fun fillFilterOptions(){
+        val filterSpinner = findViewById<Spinner>(R.id.main_filter)
+        val filterOptions = arrayOf("Marka", "Model", "Godina", "Kilometraža do")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, filterOptions)
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        filterSpinner.adapter = adapter
+
+        filterSpinner.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parentView: AdapterView<*>?, selectedItemView: View?, position: Int, id: Long) {
+                currentFilter = filterOptions[position]
+            }
+
+            override fun onNothingSelected(parentView: AdapterView<*>?) {
+            }
+        })
     }
 
 }
